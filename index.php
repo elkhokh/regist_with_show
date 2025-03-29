cc41c2dbd0401a09dc0d851e572afe94140b9aed
Mostafa Elkhokhy <147348000+elkhokh@users.noreply.github.com>
2025-03-29 03:03:23 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registration form</title>
+    <title>Registration Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
@@ -91,13 +91,7 @@ if(empty($password)){ echo "<div class='alert alert-danger'> not found password<
             </div>
         </div>
     </div>
-    <!-- <div class="col-2 mx-auto bg-light my-2  border-1">
-        
-    <div class="mb-3">
-    <h2>go to table</h2>
-                       <h2><a href="table.php">table</a></h2> 
-                    </div>
-    </div> -->
+    
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
